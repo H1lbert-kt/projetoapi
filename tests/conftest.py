886e26429a40027c5506a73321c247c82f66c81e
Hilbert Danrlley <hilbertmiguel14@gmail.com>
2026-08-26 18:29:34 -0300

    Alterações no conftest: fixtures para usuario comum e admin

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
 from app.main import app as fastapi_app, get_db
+from app.utils import gerar_hash
 from app.database import Base
 from app import models
 
@@ -42,3 +43,33 @@ def cliente(db_session):
         yield test_client
 
     fastapi_app.dependency_overrides.clear()
+
+@pytest.fixture
+def headers_usuario_comum(cliente, db_session):
+    user = models.User(
+        nome="User comum",
+        email="dev@teste.com",
+        senha=gerar_hash("123456"),
+        is_admin=False
+    )
+    db_session.add(user)
+    db_session.commit()
+
+    res = cliente.post("/login", data={"username": "dev@teste.com", "password": "123456"})
+    token = res.json()["access_token"]
+    return {"Authorization": f"Bearer {token}"}
+
+@pytest.fixture
+def headers_admin(cliente, db_session):
+    user = models.User(
+        nome="User comum",
+        email="dev@teste.com",
+        senha=gerar_hash("123456"),
+        is_admin=True
+    )
+    db_session.add(user)
+    db_session.commit()
+    
+    res = cliente.post("/login", data={"username": "dev@teste.com", "password": "123456"})
+    token = res.json()["access_token"]
+    return {"Authorization": f"Bearer {token}"}
