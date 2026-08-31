39058a0f05a0832511e0372c58b39ed31c0ace32
Hilbert Danrlley <hilbertmiguel14@gmail.com>
2026-08-31 15:39:13 -0300

    testes de cobertura

--- app/utils.py
+++ app/utils.py
@@ -1,46 +1,42 @@
-import os
-from datetime import datetime, timedelta, timezone
-from typing import Optional
 from dotenv import load_dotenv
-from jose import JWTError, jwt
+import os
+from datetime import timedelta, datetime, timezone
+from jose import jwt, JWTError
 from argon2 import PasswordHasher
-from argon2.exceptions import VerifyMismatchError, InvalidHashError
+from argon2.exceptions import VerifyMismatchError
 
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-if not SECRET_KEY:
-    raise ValueError("ERRO CRÍTICO: A variável de ambiente 'SECRET_KEY' não está definida!")
-
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+
 
 ph = PasswordHasher()
 
-def gerar_hash(senha: str) -> str:
+def gerar_hash(senha: str):
     return ph.hash(senha)
 
-def verificar_senha(senha_plana: str, senha_hasheada: str) -> bool:
-    try:
-        return ph.verify(senha_hasheada, senha_plana)
-    except (VerifyMismatchError, InvalidHashError):
-        return False
+def verificar_senha(senha_plana: str, senha_hasheada: str):
+   try:
+       return ph.verify(senha_hasheada, senha_plana)
+   
+   except VerifyMismatchError:
+       return False
 
-def criar_token_acesso(id_usuario: int) -> str:
+
+
+def criar_token_acesso(id_usuario):
     data_expiracao = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    
-    dados = {
-        "sub": str(id_usuario),
-        "exp": data_expiracao
-    }
-    
-    jwt_codificado = jwt.encode(dados, SECRET_KEY, algorithm=ALGORITHM)
+    dados = {"user_id": id_usuario,
+             "exp": data_expiracao}
+    jwt_codificado = jwt.encode(dados, SECRET_KEY, ALGORITHM)
     return jwt_codificado
 
-def verificar_token(token: str) -> Optional[str]:
+def verificar_token(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id: Optional[str] = payload.get("sub")
+        user_id = payload.get("user_id")
         return user_id
-    except JWTError:
+    except (JWTError, AttributeError):
         return None
